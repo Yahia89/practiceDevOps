00b05249ee63fe1387f4f5b70a811010ff77d858
14242 <14242@192.168.0.5>
2023-08-07 00:19:53 -0700

    practice devops weekend

---
+++ src/Training/Welcome.java
@@ -0,0 +1,22 @@
+package Training;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sub1 = 50;
+		int sub2 = 50;
+		int sub3 = 150;
+		int total = sub1 + sub2+ sub3;
+		
+System.out.println("Welcome to the code lab training");
+System.out.println("Hello everyone good afternoon");
+//System.out.println(20*40);
+System.out.println(total);
+
+
+
+	}
+
+}
